cd2a50fee09662fb018e2969d8ec1ef36a202092
Simon Brännström <simon.brannstrom1@gmail.com>
2021-05-17 21:13:34 +0300

    Fixed major issue preventing commands to run after installing oh my zsh. Swapped out wget for curl. I like to use built-in system tools whenever possible.

--- omzinstall.sh
+++ omzinstall.sh
@@ -1,7 +1,7 @@
 #For installing Oh My Zsh along with the theme powerlevel10k
 set -x #echo on
 cd ~/
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) | sed 's/exec zsh -l//g'"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed 's/exec zsh -l//g')"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 cd -
 cd ~/.oh-my-zsh/custom/themes/powerlevel10k
